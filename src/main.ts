c3a95c81f3ff57ad6d51d1c76bfb7b70da15092f
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-11-09 22:34:29 -0300

    Add vue-router dependency and configure router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.mount("#app");
