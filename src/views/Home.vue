509f37583e34ecedb1ca6b071aeee19a46f822a7
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-07-02 09:37:22 -0300

    Agregar configuración de rutas y vista principal para la página de inicio

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="home"></div>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
